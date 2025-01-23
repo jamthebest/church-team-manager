14b28902dc73845a64e3c0d550652c1bae7486ee
Javier Midence <javier.midence22@gmail.com>
2025-01-23 15:46:01 -0600

    Add schema for competitions

---
+++ src/schemas/Competition.ts
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+import { Competition } from '../types';
+import { AvailableColors } from '../constants';
+
+export const CompetitionSchema = yup.object().shape({
+    type: yup
+        .mixed<Competition['type']>()
+        .oneOf(['1 vs 1', '2 vs 2', 'Todos vs Todos', 'Individual'])
+        .required('El tipo de competencia es requerido'),
+    teams: yup
+        .array()
+        .of(
+            yup.object().shape({
+                id: yup.string().required(),
+                name: yup.string().required(),
+                color: yup.mixed<AvailableColors>().required(),
+            })
+        )
+        .required('El equipo es requerido'),
+    scores: yup
+        .array()
+        .of(yup.number().required())
+        .required('El puntaje es requerido'),
+    description: yup.string().required('La descripción es requerida'),
+});
