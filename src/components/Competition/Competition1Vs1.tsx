27538099111209d81f8310df66c90bca8980c236
Javier Midence <javier.midence22@gmail.com>
2025-01-23 15:46:46 -0600

    Create component for 1 vs 1

---
+++ src/components/Competition/Competition1Vs1.tsx
@@ -0,0 +1,162 @@
+import { useMemo } from 'react';
+import { Form, InputGroup, InputNumber, InputPicker } from 'rsuite';
+import { Controller, useFormContext } from 'react-hook-form';
+import { Team } from '../../types';
+
+export interface Competition1Vs1Props {
+    teams: Team[];
+    selectedTeams?: Team[];
+    scores?: number[];
+}
+
+const Competition1Vs1 = ({
+    teams,
+}: // selectedTeams,
+Competition1Vs1Props) => {
+    const { control, getValues, setValue } = useFormContext();
+    const usedTeams: Team[] = getValues('teams');
+    const scores: number[] = getValues('scores');
+
+    const mappedTeams = useMemo(() => {
+        return teams.reduce((acc, team) => {
+            acc[team.id] = team;
+            return acc;
+        }, {} as Record<string, Team>);
+    }, [teams]);
+    console.log({ values: usedTeams });
+
+    return (
+        <div className="space-y-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                {usedTeams.map((team, index) => (
+                    <div key={index}>
+                        <label className="block mb-1">
+                            Equipo {index + 1}:
+                        </label>
+                        <Controller
+                            name={`teams.${index}.id`}
+                            control={control}
+                            defaultValue={team.id}
+                            render={({
+                                field: { value, onChange },
+                                fieldState: { error },
+                            }) => (
+                                <Form.Group>
+                                    <InputPicker
+                                        data={teams.map((team) => ({
+                                            label: team.name,
+                                            value: team.id,
+                                        }))}
+                                        value={value}
+                                        onChange={(val: string) => {
+                                            const team = mappedTeams[val];
+                                            if (team) {
+                                                onChange(team.id);
+                                                setValue(
+                                                    `teams.${index}.name`,
+                                                    team.name
+                                                );
+                                                setValue(
+                                                    `teams.${index}.color`,
+                                                    team.color
+                                                );
+                                            }
+                                        }}
+                                        block
+                                    />
+                                    <Form.ErrorMessage
+                                        show={!!error}
+                                        placement="bottomStart"
+                                    >
+                                        {error?.message}
+                                    </Form.ErrorMessage>
+                                </Form.Group>
+                            )}
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                {usedTeams.map((team: Team, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="flex items-center space-x-2"
+                        >
+                            {team?.id && (
+                                <div
+                                    className="w-6 h-6 rounded-full"
+                                    style={{
+                                        backgroundColor: team.color,
+                                    }}
+                                ></div>
+                            )}
+                            <label
+                                className="block"
+                                style={{
+                                    maxWidth: 150,
+                                    whiteSpace: 'nowrap',
+                                }}
+                            >
+                                Puntos :
+                            </label>
+                            <Controller
+                                name={`scores.${index}`}
+                                control={control}
+                                defaultValue={scores[index]}
+                                render={({
+                                    field: { value, onChange },
+                                    fieldState: { error },
+                                }) => (
+                                    <Form.Group>
+                                        <InputGroup>
+                                            <InputGroup.Button
+                                                disabled={value === 0}
+                                                onClick={() => {
+                                                    onChange(value - 1);
+                                                }}
+                                            >
+                                                -
+                                            </InputGroup.Button>
+                                            <InputNumber
+                                                className={
+                                                    'custom-input-number'
+                                                }
+                                                value={value}
+                                                min={0}
+                                                onChange={(
+                                                    val: string | number | null
+                                                ) => {
+                                                    onChange(
+                                                        !isNaN(Number(val))
+                                                            ? Number(val)
+                                                            : 0
+                                                    );
+                                                }}
+                                            />
+                                            <InputGroup.Button
+                                                onClick={() => {
+                                                    onChange(value + 1);
+                                                }}
+                                            >
+                                                +
+                                            </InputGroup.Button>
+                                        </InputGroup>
+                                        <Form.ErrorMessage
+                                            show={!!error}
+                                            placement="bottomStart"
+                                        >
+                                            {error?.message}
+                                        </Form.ErrorMessage>
+                                    </Form.Group>
+                                )}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Competition1Vs1;
