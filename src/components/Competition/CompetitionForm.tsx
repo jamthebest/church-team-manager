513aa9b3cdbbc8bdf46030f339bc1e0463272ac3
Javier Midence <javier.midence22@gmail.com>
2025-01-28 21:24:38 -0600

    Add default value to edit

--- src/components/Competition/CompetitionForm.tsx
+++ src/components/Competition/CompetitionForm.tsx
@@ -34,7 +34,7 @@ const CompetitionForm = ({
     const methods = useForm<CompetitionInputs>({
         resolver: yupResolver(CompetitionSchema),
     });
-    const { control, register, handleSubmit, watch } = methods;
+    const { control, register, handleSubmit, setValue, watch } = methods;
     const { replace } = useFieldArray({ name: 'teams', control });
     const { replace: replaceScore } = useFieldArray({
         name: 'scores',
@@ -53,6 +53,90 @@ const CompetitionForm = ({
         );
         replaceScore(teams.map(() => 0));
     }, [competition, replace, replaceScore, teams]);
+
+    useEffect(() => {
+        if (type === '1 vs 1') {
+            replace([
+                {
+                    id: competition?.teams[0]?.id ?? '',
+                    name: competition?.teams[0]?.name ?? '',
+                    color: competition?.teams[0]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[1]?.id ?? '',
+                    name: competition?.teams[1]?.name ?? '',
+                    color: competition?.teams[1]?.color ?? '#00BCD4',
+                },
+            ]);
+            replaceScore(competition?.scores.slice(0, 2) ?? [0, 0]);
+        } else if (type === '2 vs 1') {
+            replace([
+                {
+                    id: competition?.teams[0]?.id ?? '',
+                    name: competition?.teams[0]?.name ?? '',
+                    color: competition?.teams[0]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[1]?.id ?? '',
+                    name: competition?.teams[1]?.name ?? '',
+                    color: competition?.teams[1]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[2]?.id ?? '',
+                    name: competition?.teams[2]?.name ?? '',
+                    color: competition?.teams[2]?.color ?? '#00BCD4',
+                },
+            ]);
+            replaceScore(competition?.scores.slice(0, 2) ?? [0, 0]);
+        } else if (type === '2 vs 2') {
+            replace([
+                {
+                    id: competition?.teams[0]?.id ?? '',
+                    name: competition?.teams[0]?.name ?? '',
+                    color: competition?.teams[0]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[1]?.id ?? '',
+                    name: competition?.teams[1]?.name ?? '',
+                    color: competition?.teams[1]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[2]?.id ?? '',
+                    name: competition?.teams[2]?.name ?? '',
+                    color: competition?.teams[2]?.color ?? '#00BCD4',
+                },
+                {
+                    id: competition?.teams[3]?.id ?? '',
+                    name: competition?.teams[3]?.name ?? '',
+                    color: competition?.teams[3]?.color ?? '#00BCD4',
+                },
+            ]);
+            replaceScore(competition?.scores.slice(0, 2) ?? [0, 0]);
+        } else if (type === 'Todos vs Todos') {
+            replace(
+                teams.map((team) => ({
+                    id: team.id,
+                    name: team.name,
+                    color: team.color,
+                }))
+            );
+            replaceScore(
+                competition?.scores.slice(0, teams.length) ?? teams.map(() => 0)
+            );
+        } else {
+            replace([
+                {
+                    id: competition?.teams[0]?.id ?? '',
+                    name: competition?.teams[0]?.name ?? '',
+                    color: competition?.teams[0]?.color ?? '#00BCD4',
+                },
+            ]);
+            replaceScore(competition?.scores.slice(0, 1) ?? [0]);
+        }
+        if (competition) {
+            setValue('description', competition.description);
+        }
+    }, [competition, replace, replaceScore, setValue, teams, type]);
 
     return (
         <FormProvider {...methods}>
@@ -104,86 +188,6 @@ const CompetitionForm = ({
                                             val: Competition['type']
                                         ) => {
                                             onChange(val);
-                                            if (val === '1 vs 1') {
-                                                replace([
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                ]);
-                                                replaceScore([0, 0]);
-                                            } else if (val === '2 vs 1') {
-                                                replace([
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                ]);
-                                                replaceScore([0, 0]);
-                                            } else if (val === '2 vs 2') {
-                                                replace([
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                ]);
-                                                replaceScore([0, 0]);
-                                            } else if (
-                                                val === 'Todos vs Todos'
-                                            ) {
-                                                replace(
-                                                    teams.map((team) => ({
-                                                        id: team.id,
-                                                        name: team.name,
-                                                        color: team.color,
-                                                    }))
-                                                );
-                                                replaceScore(
-                                                    teams.map(() => 0)
-                                                );
-                                            } else {
-                                                replace([
-                                                    {
-                                                        id: '',
-                                                        name: '',
-                                                        color: '#00BCD4',
-                                                    },
-                                                ]);
-                                                replaceScore([0]);
-                                            }
                                         }}
                                         block
                                     />
@@ -199,34 +203,12 @@ const CompetitionForm = ({
                     />
                 </div>
                 <div>
-                    {type === '1 vs 1' && (
-                        <Competition1Vs1
-                            teams={teams}
-                            // selectedTeams={competition?.teams}
-                            // scores={competition?.scores}
-                        />
-                    )}
-                    {type === '2 vs 1' && (
-                        <Competition2Vs1
-                            teams={teams}
-                            // selectedTeams={competition?.teams}
-                            // scores={competition?.scores}
-                        />
-                    )}
-                    {type === '2 vs 2' && (
-                        <Competition2Vs2
-                            teams={teams}
-                            // selectedTeams={competition?.teams}
-                            // scores={competition?.scores}
-                        />
-                    )}
+                    {type === '1 vs 1' && <Competition1Vs1 teams={teams} />}
+                    {type === '2 vs 1' && <Competition2Vs1 teams={teams} />}
+                    {type === '2 vs 2' && <Competition2Vs2 teams={teams} />}
                     {type === 'Todos vs Todos' && <CompetitionAllVsAll />}
                     {type === 'Individual' && (
-                        <CompetitionIndividual
-                            teams={teams}
-                            // selectedTeams={competition?.teams}
-                            // scores={competition?.scores}
-                        />
+                        <CompetitionIndividual teams={teams} />
                     )}
                 </div>
                 <div>
@@ -256,21 +238,23 @@ const CompetitionForm = ({
                         )}
                     />
                 </div>
-                <button
-                    type="submit"
-                    className="w-full p-2 bg-green-500 text-white rounded"
-                >
-                    Guardar
-                </button>
-                {competition?.id && (
+                <div className="flex justify-end space-x-4">
+                    {competition?.id && (
+                        <button
+                            type="button"
+                            onClick={() => onDelete(competition.id as string)}
+                            className="p-2 bg-red-500 text-white rounded"
+                        >
+                            Eliminar
+                        </button>
+                    )}
                     <button
-                        type="button"
-                        onClick={() => onDelete(competition.id as string)}
-                        className="w-full p-2 bg-red-500 text-white rounded"
+                        type="submit"
+                        className="p-2 bg-green-500 text-white rounded"
                     >
-                        Eliminar
+                        Guardar
                     </button>
-                )}
+                </div>
             </form>
         </FormProvider>
     );
